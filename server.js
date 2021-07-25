25359aaf992a9edd896b9121743030c9f7ab4def
Reuben <rleibgenkin@gmail.com>
2021-07-25 05:14:03 -0400

    changes to surver js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-//const base64 = require('js-base64');
 let http = require('http');
 const { Buffer } = require('buffer');
 const app = express();
@@ -38,23 +37,22 @@ spotifyApi.clientCredentialsGrant().then(
     process.env['spotifyAccess'] = data.body.access_token;
 
     //setting access token with node pkg
-    //in testing to see if its needed
-    //spotifyApi.setAccessToken('data.body.access_token');
-
+    //spotifyApi.setAccessToken(data.body.access_token);
+    //console.log(spotifyApi.access_token)
     //console logs to give access info to dev
     console.log('The access token expires in ' + data.body['expires_in']);
     console.log('The access token is ' + data.body['access_token']);
   })
 
 //track search, needs variable to pass in from input
-spotifyApi.searchTracks('???')
-  .then(function (data) {
-    console.log('Search by "???"', data.body);
-  }, function (err) {
-    console.error(err);
-  });
+// spotifyApi.searchTracks('???')
+//   .then(function (data) {
+//     console.log('Search by "???"', data.body);
+//   }, function (err) {
+//     console.error(err);
+//   });
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening on port ' + PORT));
 });
