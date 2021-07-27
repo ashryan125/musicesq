f67b312134231bd624a7a8006eb53347a140e95c
Reuben <rleibgenkin@gmail.com>
2021-07-27 02:43:57 -0400

    changes to post routes to work with the new additions

--- public/javascript/input.js
+++ public/javascript/input.js
@@ -33,7 +33,7 @@ async function apiCall(event) {
 
                     wrapper = document.createElement("DIV");
                     wrapper.setAttribute("id", "autocomplete-list");
-                    listParent.parentNode.appendChild(wrapper);
+                    listParent.append(wrapper);
 
                     let parData = JSON.parse(results);
                     console.log(parData)
@@ -43,18 +43,18 @@ async function apiCall(event) {
                         console.log(element.track.length);
                         let b = document.createElement("DIV");
                         b.setAttribute("class", "inputItem");
-                        /*make the matching letters bold:*/
+                        //make the matching letters bold:
                         b.innerHTML = "<strong>" + element.track.substr(0, element.track.length) + "   -    " + element.artistName + "</strong>";
                         b.innerHTML += element.track.substr(element.track.length);
                         b.innerHTML += "<input type='hidden' value='" + element.track + "'>";
-                        /*execute a function when someone clicks on the item value (DIV element):*/
+                        //execute a function when someone clicks on the item value (DIV element):
 
                         wrapper.appendChild(b);
                         b.addEventListener("click", function (e) {
-                            /*insert the value for the autocomplete text field:*/
+                            //insert the value for the autocomplete text field:
                             searchInput.value = this.getElementsByTagName("input")[0].value;
-                            /*close the list of autocompleted values,
-                            (or any other open lists of autocompleted values:*/
+                            //*close the list of autocompleted values,
+                            
                             console.log(element.artistName)
                             console.log(element.extUrl.spotify)
                             artistNameInput.value = element.artistName;
@@ -77,18 +77,18 @@ async function apiCall(event) {
 }
 
 function addActive(x) {
-    /*a function to classify an item as "active":*/
+    //a function to classify an item as "active":
     if (!x) return false;
-    /*start by removing the "active" class on all items:*/
+    //start by removing the "active" class on all items:
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
+    //add class "autocomplete-active":
     x[currentFocus].classList.add("autocomplete-active");
 }
 
 function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
+    //a function to remove the "active" class from all autocomplete items:
     for (var i = 0; i < x.length; i++) {
         x[i].classList.remove("autocomplete-active");
     }
@@ -101,7 +101,7 @@ function closeAllLists(elmnt) {
     document.querySelectorAll(".inputItem").forEach(el => el.remove());
 }
 
-/*execute a function when someone clicks in the document:*/
+//execute a function when someone clicks in the document:
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
 });
